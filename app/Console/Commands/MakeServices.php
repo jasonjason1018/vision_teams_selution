eb7e7a8192faa59539bcb9d4a350ea81b6f5aae7
jason <jasonjason1018@gmail.com>
2024-08-28 11:05:31 +0800

    feat: 建立新增Services Command

---
+++ app/Console/Commands/MakeServices.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class MakeServices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $path = app_path("Services/{$name}.php");
+
+        if (File::exists($path)) {
+            $this->error("Service {$name} already exists!");
+            return;
+        }
+
+        $template = "<?php
+
+namespace App\Services;
+
+class {$name}
+{
+    //
+}
+";
+
+        File::ensureDirectoryExists(app_path('Services'));
+        File::put($path, $template);
+
+        $this->info("Service {$name} created successfully.");
+    }
+}
+
